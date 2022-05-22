06f9c86e8c0696e236bac10ee3ba20a3f167fe8f
mktwohy <69822801+mktwohy@users.noreply.github.com>
2022-05-21 22:06:34 -0500

    add paste button

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,21 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.DesktopMaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.ClipboardManager
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -11,7 +24,28 @@ import androidx.compose.ui.window.application
 @Preview
 fun App() {
     DesktopMaterialTheme {
-        Text(BionicReader("Hello World!").annotatedString)
+        val clipboardManager: ClipboardManager = LocalClipboardManager.current
+        var pastedText by remember { mutableStateOf(buildAnnotatedString {  }) }
+
+        Box(
+            contentAlignment = Alignment.Center,
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = {
+                        clipboardManager.getText()?.let { pastedText = it }
+                    }
+                ) {
+                    Text("Paste")
+                }
+                Row(horizontalArrangement = Arrangement.Center) {
+                    Text(pastedText)
+                    Text(BionicReader(pastedText.text).annotatedString)
+                }
+            }
+        }
     }
 }
 
